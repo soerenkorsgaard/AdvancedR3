94db9ba1a1422a54548b26f37eb2345bf15d48d5
Søren K. Martiny <soren.korsgaard@clin.au.dk>
2024-12-10 16:03:27 +0100

    Adding basic statistics function (descriptive statistics of numeric variables)

---
+++ R/functions.R
@@ -0,0 +1,12 @@
+#' Descriptive statistics - obtain mean and sd of numeric variables by a grouping variable
+#'
+#' @param data A dataset
+#'
+#' @return A data.frame/tibble
+
+descriptive_stats <- function(data) {
+  data |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ round(.x, digits = 1)))
+}

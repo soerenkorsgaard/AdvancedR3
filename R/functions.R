b14d736aa28c26ddea6aed9a697abff9964779c9
Søren K. Martiny <soren.korsgaard@clin.au.dk>
2024-12-12 11:27:39 +0100

    Make a function to visualize estimates, add to target, run tar_make

--- R/functions.R
+++ R/functions.R
@@ -171,3 +171,22 @@ calculate_estimates <- function(data) {
     dplyr::distinct(metabolite, term) |>
     dplyr::right_join(model_estimates, by = "term")
 }
+
+# Plot estimates for each metabolite -------------------------------------------------------
+
+#' Plot the estimates and standard errors of the model results.
+#'
+#' @param results The model estimate results.
+#'
+#' @return A ggplot2 figure
+plot_estimates <- function(results) {
+    results |>
+        ggplot2::ggplot(ggplot2::aes(
+            x = estimate,
+            y = metabolite,
+            xmin = estimate - std.error,
+            xmax = estimate + std.error
+        )) +
+        ggplot2::geom_pointrange() +
+        ggplot2::coord_fixed(xlim = c(0, 5))
+}
